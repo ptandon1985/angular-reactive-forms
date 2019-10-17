8243e69d912728612e2e4465b5e8259433e67927
ptandon1985 <ptandon1985@gmail.com>
2019-10-17 15:11:39 -0500

    Dynamic Control  Reactive Form

--- src/app/components/form-builder/form-builder.component.ts
+++ src/app/components/form-builder/form-builder.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Validators } from '@angular/forms';
+import { FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-form-builder',
@@ -20,8 +21,14 @@ export class FormBuilderComponent implements OnInit {
       address2: [''],
       state: [''],
       zip: ['']
-    })
+    }),
+
+    mobiles: this.fb.array([
+      this.fb.control('')
+    ])
   });
+
+  counter: number;
 
   onSubmit () {
     console.warn(this.userProfileForm.value);
@@ -29,6 +36,23 @@ export class FormBuilderComponent implements OnInit {
     console.warn('Last Name: '+this.userProfileForm.get('lastName').value);
     console.warn('Address 1: '+this.userProfileForm.get(['address','address1']).value);
     console.warn('Address 2: '+this.userProfileForm.get('address').get('address2').value);
+
+    console.warn('Mobile 1: '+this.userProfileForm.get(['mobiles', '0']).value);
+
+    this.counter=0;
+
+    for (let mob of this.mobiles.controls) {
+      console.log('Mobile Number: '+this.userProfileForm.get(['mobiles', this.counter]).value);
+      this.counter = this.counter+1;
+    }
+  }
+
+  get mobiles() {
+    return this.userProfileForm.get('mobiles') as FormArray;
+  }
+  
+  addMobileNumber() {
+    this.mobiles.push(this.fb.control(''));
   }
 
   ngOnInit() {
